b125c0c44952640edeaffcfd502ffb5ce05933b8
engmagdy87 <eng.magdy87@gmail.com>
2017-11-15 01:44:48 +0200

    add exoplayer to play videos and handle null videos issue

--- app/src/main/java/com/baking/mm/bakingapp/RecipeStepsNav.java
+++ app/src/main/java/com/baking/mm/bakingapp/RecipeStepsNav.java
@@ -1,12 +1,14 @@
 package com.baking.mm.bakingapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +16,30 @@ import android.widget.TextView;
 import com.baking.mm.bakingapp.javacalsses.RecipeMediaFragment;
 import com.baking.mm.bakingapp.javacalsses.RecipeStepFragment;
 import com.baking.mm.bakingapp.pojo.RecipeSteps;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.LoopingMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,7 +72,6 @@ public class RecipeStepsNav extends AppCompatActivity {
             }
             if (extras.containsKey("steps")) {
                 recipeSteps = myIntent.getParcelableArrayListExtra("steps");
-
             }
             if (extras.containsKey("clicked_item")) {
                 index = myIntent.getIntExtra("clicked_item",0);
@@ -62,8 +87,6 @@ public class RecipeStepsNav extends AppCompatActivity {
         recipeStepsFragment.setIndex(index);
 
         counter = findViewById(R.id.tv_steps_counter);
-
-
         counter.setText(String.valueOf(index+1)+"/"+recipeSteps.size());
         next = findViewById(R.id.btn_next);
         previous = findViewById(R.id.btn_prev);
@@ -107,6 +130,5 @@ public class RecipeStepsNav extends AppCompatActivity {
 
         fragmentManager.beginTransaction()
                 .add(R.id.step_container, recipeStepsFragment).commit();
-
     }
 }
--- app/src/main/java/com/baking/mm/bakingapp/javacalsses/RecipeMediaFragment.java
+++ app/src/main/java/com/baking/mm/bakingapp/javacalsses/RecipeMediaFragment.java
@@ -1,16 +1,38 @@
 package com.baking.mm.bakingapp.javacalsses;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.baking.mm.bakingapp.R;
 import com.baking.mm.bakingapp.pojo.RecipeSteps;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 
 import java.util.List;
 
@@ -18,37 +40,41 @@ import java.util.List;
  * Created by MM on 11/8/2017.
  */
 
-public class RecipeMediaFragment extends Fragment {
+public class RecipeMediaFragment extends Fragment implements ExoPlayer.EventListener {
     public List<RecipeSteps> mRecipeSteps;
-    TextView textView;
+    SimpleExoPlayerView videoView;
     public int index;
 
-    public RecipeMediaFragment() {
-    }
+    SimpleExoPlayer player;
+    String path;
+    View rootView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_media, container, false);
-        textView = rootView.findViewById(R.id.tv_recipe_video);
+        rootView = inflater.inflate(R.layout.fragment_media, container, false);
+        videoView = rootView.findViewById(R.id.player_view);
+
         if (mRecipeSteps != null) {
             if (mRecipeSteps.get(index).videoURL.toString().isEmpty() || mRecipeSteps.get(index).videoURL.toString().equals(""))
-                textView.setText("No Video");
+                path="";
             else
-                textView.setText(mRecipeSteps.get(0).videoURL.toString());
+                path = mRecipeSteps.get(index).videoURL.toString();
         }
+        videoPlayer(rootView);
         return rootView;
 
     }
 
     public void changeFragment() {
+        this.onDestroy();
         if (mRecipeSteps != null) {
             if (mRecipeSteps.get(index).videoURL.toString().isEmpty() || mRecipeSteps.get(index).videoURL.toString().equals(""))
-                textView.setText("No Video");
+                path="";
             else
-                textView.setText(mRecipeSteps.get(index).videoURL.toString());
-
+                path = mRecipeSteps.get(index).videoURL.toString();
         }
+        videoPlayer(rootView);
     }
 
     public void setIndex(int index) {
@@ -59,5 +85,78 @@ public class RecipeMediaFragment extends Fragment {
         this.mRecipeSteps = mRecipeSteps;
     }
 
+    public void videoPlayer(View view){
 
+        TrackSelector trackSelector = new DefaultTrackSelector();
+        LoadControl loadControl = new DefaultLoadControl();
+
+        player = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+
+        SimpleExoPlayerView playerView = view.findViewById(R.id.player_view);
+        playerView.setPlayer(player);
+        playerView.setKeepScreenOn(true);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(getContext(), "Android-ExoPlayer");
+
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        MediaSource videoSource = new ExtractorMediaSource(Uri.parse(path), dataSourceFactory, extractorsFactory, null, null);
+
+        player.addListener(this);
+        player.prepare(videoSource);
+        playerView.requestFocus();
+        player.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (player != null) {
+            player.setPlayWhenReady(false);
+        }
+    }
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        switch (playbackState) {
+            case ExoPlayer.STATE_BUFFERING:
+                break;
+            case ExoPlayer.STATE_IDLE:
+                break;
+            case ExoPlayer.STATE_READY:
+                break;
+            case ExoPlayer.STATE_ENDED:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.release();
+    }
 }
